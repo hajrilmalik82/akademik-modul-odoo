50045292116be372fa0b6108350e9dd6fbf46567
hajrilmalik82 <hajrilmalik82@gmail.com>
2026-02-04 10:48:07 +0700

    nomor 3

--- akademik_dosen/__manifest__.py
+++ akademik_dosen/__manifest__.py
@@ -5,7 +5,11 @@
     'author': 'Hajril',
     'depends': ['base', 'hr', 'sistem_akademik'],
     'data': [
+        'security/akademik_security.xml',
+        'security/ir.model.access.csv',
+        'security/akademik_rules.xml',
         'views/hr_employee_view.xml',
+        'views/menu_security.xml',
     ],
     'assets': {
         'web.assets_backend': [
